6cd11a81a68dd3a9daf7c81f71b32156ba9a89f2
Indhucherukuri <cherukuriindhu@gmail.com>
2026-08-13 17:31:58 +0530

    Create init_database.sql for DataWarehouse setup

---
+++ Scripts/init_database.sql
@@ -0,0 +1,12 @@
+        --- CREATE DATABASE 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse; 
+
+USE DataWarehouse;
+
+CREATE SCHEMA Bronze;
+CREATE SCHEMA Silver;
+CREATE SCHEMA Gold;
+
